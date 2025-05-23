bda1a30d01f5f3a7b3fbbf4d488307c30911c4b6
Lucas Vervloessem <lucas.vervloessem.atc@gmail.com>
2025-05-23 12:30:48 +0200

    fix test

--- backend/authentification/urls.py
+++ backend/authentification/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
-    path("_allauth/", include("allauth.headless.urls")),
+    path("", include("allauth.headless.urls")),
 ]
